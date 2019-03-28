51fa6b229b861e90d8f4aa3100db81c1fd76ff44
SOYEONG OH <axionoso@gmail.com>
2019-03-28 15:52:54 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,15 +1,15 @@
 class Hash
   def keys_of(*arguments)
     arr = Array.new
-    if arguments == "Panama"
-      arr << "red-footed tortoise"
-    elsif arguments == "Madagascar"
-      arr << "aye-aye"
-      arr << "tomato frog"
-    elsif arguments == "Australia"
-      arr << "sugar glider"
-      arr << "kangaroo"
-      arr << "koala"
+    # if arguments == "Panama"
+    #   arr << "red-footed tortoise"
+    # elsif arguments == "Madagascar"
+    #   arr << "aye-aye"
+    #   arr << "tomato frog"
+    # elsif arguments == "Australia"
+    #   arr << "sugar glider"
+    #   arr << "kangaroo"
+    #   arr << "koala"
     end
   end
 end
