7fca8294ad67dfcc46de9ad497e88c2edaaa1e8d
SOYEONG OH <axionoso@gmail.com>
2019-03-28 15:56:55 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,8 +2,13 @@ class Hash
   def keys_of(*arguments)
     arr = []
     arguments.each do |arg|
-
+      self.each do |k, v|
+        if v == arg
+          arr << k
+        end
+      end
     end
+    return arr
     # if arguments == "Panama"
     #   arr << "red-footed tortoise"
     # elsif arguments == "Madagascar"
